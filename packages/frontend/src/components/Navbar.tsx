f606a5bdc52e808c5c5cbdf077d4db1dd004c75a
oveddan <stangogh@gmail.com>
2022-07-16 18:39:45 -0700

    fixed title links

--- packages/frontend/src/components/Navbar.tsx
+++ packages/frontend/src/components/Navbar.tsx
@@ -49,7 +49,7 @@ const Navbar = () => (
               className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
               aria-current="page"
             >
-              Home
+              Explore
             </a>
           </li>
           <li>
@@ -57,23 +57,7 @@ const Navbar = () => (
               href="#"
               className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
             >
-              About
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-            >
-              Services
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-            >
-              Contact
+              Your Worlds 
             </a>
           </li>
         </ul>
