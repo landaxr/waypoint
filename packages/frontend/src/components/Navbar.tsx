f80d8e165d4ce22aa0d5d4cf30b1abdbe8fd6f60
Dan Oved <stangogh@gmail.com>
2022-07-16 22:46:13 -0700

    web3 login with wagmi and new icons title (#2)
    * added login, and formatted everything prettier
    
    * added front-end login
    
    * added login buttons and showing status and ens name
    
    * fixed title links
    
    * new front-end and icon and title
    
    * updated yarn.lock
    
    Co-authored-by: danoved <danoved@chavas-mbp.lan>

--- packages/frontend/src/components/Navbar.tsx
+++ packages/frontend/src/components/Navbar.tsx
@@ -1,5 +1,10 @@
 import Web3Login from "./Web3Login";
 
+const menuItems: { link: string; title: string }[] = [
+  { link: "/explore", title: "Explore" },
+  { link: "/your-worlds", title: "Your worlds" },
+];
+
 const Navbar = () => (
   <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
     <div className="container flex flex-wrap justify-between items-center mx-auto">
@@ -43,23 +48,19 @@ const Navbar = () => (
         id="navbar-cta"
       >
         <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-          <li>
-            <a
-              href="#"
-              className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-              aria-current="page"
-            >
-              Explore
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-            >
-              Your Worlds 
-            </a>
-          </li>
+          {menuItems.map((menuItem) => (
+            <>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                  aria-current="page"
+                >
+                  {menuItem.title}
+                </a>
+              </li>
+            </>
+          ))}
         </ul>
       </div>
     </div>
