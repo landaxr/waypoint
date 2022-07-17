ed72c6d6a161effde75f536f3739709541bd2a51
danoved <danoved@chavas-mbp.lan>
2022-07-16 17:49:35 -0700

    added login, and formatted everything prettier

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return ( 
-  <h1 className="text-3xl font-bold underline">
-    hello world
-  </h1> 
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline">hello world</h1>
+    </>
   );
 }
 
