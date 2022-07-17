18d671b2e800a0ca849e411fa70a998909eb143f
oveddan <stangogh@gmail.com>
2022-07-16 18:05:56 -0700

    added front-end login

--- packages/frontend/src/components/Navbar.tsx
+++ packages/frontend/src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import LoginButton from "./LoginButton";
+
 const Navbar = () => (
   <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
     <div className="container flex flex-wrap justify-between items-center mx-auto">
@@ -12,12 +14,7 @@ const Navbar = () => (
         </span>
       </a>
       <div className="flex md:order-2">
-        <button
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Get started
-        </button>
+        <LoginButton />
         <button
           data-collapse-toggle="navbar-cta"
           type="button"
