e0be9f27b79aaf3db5e28d7d25629a141f3e9ce2
oveddan <stangogh@gmail.com>
2022-07-26 11:12:30 -0700

    Showing owner if has an ens name

--- src/components/World/YourWorlds.tsx
+++ src/components/World/YourWorlds.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useAccount } from "wagmi";
+import { useAccount, useEnsName } from "wagmi";
 import { useHttpsUriForIpfs } from "../../api/ipfs/ipfsUrlUtils";
 import {
   useErc721TokenForFileUrl,
@@ -8,6 +8,19 @@ import {
   WorldData,
 } from "../../api/theGraph/worldsQueries";
 import MainNavbar from "../Nav/MainNavbar";
+
+export const AccountDescriptionText = ({address}:{address: string}) => {
+  const { data, isError, isLoading } = useEnsName({
+    address,
+  })
+
+  if (isLoading) return <>Fetching name…</>
+  if (isError) return <>Error fetching name</>
+  if (data)
+  return <>{data}</>
+
+  return <>{address}</>
+}
 
 export const World = ({ world }: { world: WorldData }) => {
   // const { loading, error, data } = useQuery<WorldsData>(GET_LOCAL_WORLDS);
@@ -32,7 +45,7 @@ export const World = ({ world }: { world: WorldData }) => {
             {erc721Token?.name} (token {world.id})
           </h5>
         </Link>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{`owned by ${world.owner.id}`}</p>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 truncate ...">Owner: <AccountDescriptionText address={world.owner.id} /></p>
       </div>
     </div>
   );
